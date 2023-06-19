ce2b5ee1f96be9f466e22c5de73488ffe710f258
KelLynnP <130242733+KelLynnP@users.noreply.github.com>
2023-06-19 15:28:31 -0700

    Update BLE_Implementation.ino
    Adding all sensors... ICM not working

--- BLE_Implementation/BLE_Implementation.ino
+++ BLE_Implementation/BLE_Implementation.ino
@@ -5,10 +5,13 @@
 #include <Adafruit_GPS.h>
 #include <ArduinoJson.h>
 #include <unordered_map>
-#include <string>
 #include <SoftwareSerial.h>
 #include <vector>
 #include <string>
+
+#include "Adafruit_PM25AQI.h"
+#include <Adafruit_BME680.h>
+#include <Adafruit_ICM20948.h>
 
 
 // ---------------------------------------------------------------//
@@ -27,7 +30,6 @@ Adafruit_GPS GPS(&GPSSerial);
 uint32_t GpsTimer = millis();
 uint32_t GlobalTimer = millis();
 const int sampleRateGps = 5000;  // update line in initiation if this changes
-bool FirstTime = false;          // ugh so annoying for pre-allocating
 
 struct GPSData {
   char FullTimeStamp[20];
@@ -38,45 +40,29 @@ struct GPSData {
   bool dataReceived = false; // set true if set correctly
 };
 
-float convertDegreeMin2Dec(float DegreeMin) {
-  int degree = (int)DegreeMin;
-  float minutes = DegreeMin - degree;
-  float decimalMinutes = degree + minutes;
-  return decimalMinutes;
-}
-
 GPSData readAndStoreGPS() {
   GPSData TempGPS;
   char c = GPS.read();  // BECAUSE OF GPS LOGIC THIS NEEDS TO BE CALLED AT LEAST TWICE A SECOND... This means global delay functions are OUT   // Serial.print(c); // SHOWS ALL THE NMEA STRINGS! PRINT AT UR OWN RISK :)
   // Serial.println(c);
   if (GPSECHO) {
     if (c) {
-      // Serial.print(c);  // if a sentence is received, we can check the checksum, parse it...
+      Serial.print(c);  // if a sentence is received, we can check the checksum, parse it...
     }
   }
 
   if (GPS.newNMEAreceived()) {  // a tricky thing here is if we print the NMEA sentence, or data // we end up not listening and catching other sentences! // so be very wary if using OUTPUT_ALLDATA and trying to print out data     // Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
     if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to fals
       return TempGPS;
-    // Serial.print(GPS.seconds);
-    // Serial.println("-> seconds! ");
   }
 
   if ((millis() - GpsTimer > sampleRateGps) && (!GPS.parse(GPS.lastNMEA())) && (GPS.fix != 0)) {
     GpsTimer = millis();  // reset the timer
     sprintf(TempGPS.FullTimeStamp, "%02d:%02d:%02d %02d/%02d/%04d", GPS.hour, GPS.minute, GPS.seconds, GPS.day, GPS.month, GPS.year);
     sprintf(TempGPS.ShortTimeStamp, "%02d%02d%02d", GPS.hour, GPS.minute, GPS.seconds);
-    TempGPS.latitude =  convertDegreeMin2Dec(GPS.latitude);
-    TempGPS.longitude = convertDegreeMin2Dec(GPS.longitude);
+    TempGPS.latitude =  GPS.latitudeDegrees;
+    TempGPS.longitude = GPS.longitudeDegrees;
     TempGPS.altitude = GPS.altitude;
     TempGPS.dataReceived = true;
-    // ~~~~~~~~~~~~~~~~~~~ Run these lines to debug ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-    Serial.print(TempGPS.latitude); Serial.println(TempGPS.longitude);
-    // Serial.print(GPS.seconds);
-    // Serial.println("-> seconds! ");
-    // Serial.print(GPS.hour);Serial.println("-> hour! ");
-    // Serial.print(GPS.year);Serial.println("-> year! ");
-    // Serial.println(TempGPS.TimeStamp);
     return TempGPS;
   } else {  // settting up a Dummy vector    // Serial.print((!GPS.parse(GPS.lastNMEA())));
     GPSData TempGPS;
@@ -89,7 +75,6 @@ GPSData readAndStoreGPS() {
     return TempGPS;
   }
 }
-
 
 // ---------------------------------------------------------------//
 // ------------------ BLE information ----------------------------//
@@ -165,32 +150,78 @@ BLECharacteristic* intializeBLECharacteristic(BLECharacteristic* pCharacteristic
 }
 
 // ---------------------------------------------------------------//
-// ------------- Sensor Helper Functions -------------------------//
+// -------- Sensor Declarations + Helper Functions ---------------//
 // ---------------------------------------------------------------//
+
+Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
+PM25_AQI_Data PM25Data;
+Adafruit_BME680 bme; // Initialize the BME688 sensor object
+Adafruit_ICM20948 icm; // Initialize the ICM209448 sensor object
+unsigned long PlantMillis = 0;
+const unsigned long PlantSample = 500;
+
+struct ICMData{
+  float AccelX;  // acceleration
+  float AccelY; 
+  float AccelZ; 
+};
+
+ICMData getICMData();
+
+ICMData getICMData(){
+  ICMData ICM;
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t mag;
+  sensors_event_t temp;
+  icm.getEvent(&accel, &gyro, &temp, &mag);
+  // GTemp = temp.temperature; // not pulling a second temperature value but we could
+
+  ICM.AccelX = accel.acceleration.x; // acceleration
+  ICM.AccelY = accel.acceleration.y;
+  ICM.AccelZ = accel.acceleration.z;
+  return ICM;
+}
 
 std::vector<std::string> PullAndTranscribeData(const GPSData& GPSData2Transmit) {
   std::vector<std::string> sensorDataVector(9);
+  ICMData ICM = getICMData(); 
+
+  // GPS Data All pulled seperatly 
   sensorDataVector[0] = GPSData2Transmit.FullTimeStamp;
+
   sensorDataVector[1] = FormatAndAppendTimestamp(GPSData2Transmit.latitude, GPSData2Transmit.ShortTimeStamp);
   sensorDataVector[2] = FormatAndAppendTimestamp(GPSData2Transmit.longitude, GPSData2Transmit.ShortTimeStamp);
-  sensorDataVector[3] =  FormatAndAppendTimestamp(GPSData2Transmit.altitude, GPSData2Transmit.ShortTimeStamp);
-  // sensorDataVector[4] = "PM25";
-  // sensorDataVector[5] = "RelativeHumidity";
-  // sensorDataVector[6] = "Temperature";
-  // sensorDataVector[7] = "AccelerationX";
-  // sensorDataVector[8] = "AccelerationY";
-  // sensorDataVector[9] = "AccelerationZ";
+  sensorDataVector[3] = FormatAndAppendTimestamp(GPSData2Transmit.altitude, GPSData2Transmit.ShortTimeStamp);
+  sensorDataVector[4] = FormatAndAppendTimestamp(float(PM25Data.pm25_env), GPSData2Transmit.ShortTimeStamp);
+  
+  //relative Humidity 
+  sensorDataVector[5] = FormatAndAppendTimestamp(bme.readHumidity(), GPSData2Transmit.ShortTimeStamp);
+  sensorDataVector[6] = FormatAndAppendTimestamp(bme.readTemperature(), GPSData2Transmit.ShortTimeStamp);
+
+  // Accerleration Data
+  // sensorDataVector[7] = FormatAndAppendTimestamp(ICM.AccelX, GPSData2Transmit.ShortTimeStamp);
+  // sensorDataVector[8] = FormatAndAppendTimestamp(ICM.AccelX, GPSData2Transmit.ShortTimeStamp);
+  // sensorDataVector[9] = FormatAndAppendTimestamp(ICM.AccelZ, GPSData2Transmit.ShortTimeStamp);
 
   return sensorDataVector;
 }
 
 std::string FormatAndAppendTimestamp(float RawData, const char* TimeSnip) {
   char Data[15];
+  // Serial.println(RawData);
   sprintf(Data, ",%f", RawData);
   std::string str = std::string(TimeSnip) + Data;
-  Serial.print(str.c_str());
+  Serial.println(str.c_str());
   return str;
 }
+
+// ---------------------------------------------------------------//
+// ---------------------- Button Functions -----------------------//
+// ---------------------------------------------------------------//
+
+const int buttonPin = 25;  // the number of the pushbutton pin
+int buttonState = 0;
 
 void setup() {
 
@@ -200,6 +231,116 @@ void setup() {
   GPSSerial.begin(9600);  // #fixme (should I be worried about this?)
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);  // 1 Hz update rate
+
+  //---------- PM 2.5 Set Up ------------ //
+    // Wait one second for sensor to boot up!
+  delay(1000);
+  if (! aqi.begin_I2C()) {      // connect to the sensor over I2C
+    Serial.println("Could not find PM 2.5 sensor!");
+    while (1) delay(10);
+  }
+  Serial.println("PM25 found!");
+
+  //---------- BME 688 Begin -----------//
+    if (!bme.begin()) {
+    Serial.println("Could not find BME688 sensor!");
+  }
+  else{    // ~if~ we find Set up oversampling and filter initialization
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setPressureOversampling(BME680_OS_4X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    bme.setGasHeater(320, 150); // 320*C for 150 ms
+  }
+
+  //---------- ICM  Begin -----------//
+  if (!icm.begin_I2C()) { // if (!icm.begin_SPI(ICM_CS)) { // if (!icm.begin_SPI(ICM_CS, ICM_SCK, ICM_MISO, ICM_MOSI)) {
+
+    Serial.println("Failed to find ICM20948 chip");
+    while (1) {
+      delay(10);
+      break;
+    }
+  }
+  else{
+  Serial.println("ICM20948 Found!");
+  // icm.setAccelRange(ICM20948_ACCEL_RANGE_16_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (icm.getAccelRange()) {
+  case ICM20948_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case ICM20948_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case ICM20948_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case ICM20948_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  // Serial.println("OK");
+  // icm.setGyroRange(ICM20948_GYRO_RANGE_2000_DPS);
+  Serial.print("Gyro range set to: ");
+  switch (icm.getGyroRange()) {
+  case ICM20948_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  }
+
+  //  icm.setAccelRateDivisor(4095);
+  uint16_t accel_divisor = icm.getAccelRateDivisor();
+  float accel_rate = 1125 / (1.0 + accel_divisor);
+
+  Serial.print("Accelerometer data rate divisor set to: ");
+  Serial.println(accel_divisor);
+  Serial.print("Accelerometer data rate (Hz) is approximately: ");
+  Serial.println(accel_rate);
+
+  //  icm.setGyroRateDivisor(255);
+  uint8_t gyro_divisor = icm.getGyroRateDivisor();
+  float gyro_rate = 1100 / (1.0 + gyro_divisor);
+
+  Serial.print("Gyro data rate divisor set to: ");
+  Serial.println(gyro_divisor);
+  Serial.print("Gyro data rate (Hz) is approximately: ");
+  Serial.println(gyro_rate);
+
+  // icm.setMagDataRate(AK09916_MAG_DATARATE_10_HZ);
+  Serial.print("Magnetometer data rate set to: ");
+  switch (icm.getMagDataRate()) {
+  case AK09916_MAG_DATARATE_SHUTDOWN:
+    Serial.println("Shutdown");
+    break;
+  case AK09916_MAG_DATARATE_SINGLE:
+    Serial.println("Single/One shot");
+    break;
+  case AK09916_MAG_DATARATE_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case AK09916_MAG_DATARATE_20_HZ:
+    Serial.println("20 Hz");
+    break;
+  case AK09916_MAG_DATARATE_50_HZ:
+    Serial.println("50 Hz");
+    break;
+  case AK09916_MAG_DATARATE_100_HZ:
+    Serial.println("100 Hz");
+    break;
+  }
+  Serial.println();
+  }
+
 
   //----------- BLE Set up-------------- //
   // 1. Service information to run once   
@@ -233,23 +374,19 @@ void loop() {
   GPSData DummyGPSData = readAndStoreGPS();  // Hello GPS are you there
   
   if (DummyGPSData.dataReceived == true) {  // Essential GPS Logic!
-    GPSData GPS2Transmit = DummyGPSData;
-    Serial.println(GPS2Transmit.ShortTimeStamp);
-    // Serial.println(GPS2Transmit.latitude);
-    // Serial.println(GPS2Transmit.longitude);
-    // Serial.println(GPS2Transmit.altitude);
+    GPS2Transmit = DummyGPSData;
   }
 
- if ((deviceConnected) && (millis() - GlobalTimer > sampleRateGps)) {
+//  if ( (millis() - GlobalTimer > sampleRateGps)) { // (millis() - GlobalTimer > sampleRateGps
+  if ((deviceConnected) && (millis() - GlobalTimer > sampleRateGps)) {
     GlobalTimer = millis();
-    
+
     std::vector<std::string> EncodedData = PullAndTranscribeData(GPS2Transmit);
-    // char** EncodedData[9] = PullAndTranscribeData(GPS2Transmit);
 
     for( int i = 0 ; i < 1 ; i ++){ //NumCharacteristics
     Serial.println(EncodedData[i].c_str());
-    // pCharacteristicChars[i]->setValue(std::string(EncodedData[i]));
-    // pCharacteristicChars[i]->notify();
+    pCharacteristicChars[i]->setValue(std::string(EncodedData[i]));
+    pCharacteristicChars[i]->notify();
     }
 
   }
@@ -268,3 +405,6 @@ void loop() {
     oldDeviceConnected = deviceConnected;
   }
 }
+
+
+
