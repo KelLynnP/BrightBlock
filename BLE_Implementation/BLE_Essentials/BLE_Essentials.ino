0b5713148d88ca66c4c7a21e03c4a4738fa7d8d7
KelLynnP <130242733+KelLynnP@users.noreply.github.com>
2023-06-18 20:12:18 -0700

    Moving BLE_Essentials into Implmentation!

--- BLE_Implementation/BLE_Essentials/BLE_Essentials.ino
+++ BLE_Implementation/BLE_Essentials/BLE_Essentials.ino
@@ -2,32 +2,40 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <unordered_map>
+#include <string>
 
-// Just once 
+// Initialized Once // Not Characterisitic Specific  
 static BLEUUID BLESERVICE_UUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
 const int numServices = 30;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 BLEServer* pServer = NULL;                    // Pointer to the server
 int value = 0;
-BLE2902* pBLE2902_1;                          // Pointer to BLE2902 of Characteristic 1
-BLE2902* pBLE2902_2;                          // Pointer to BLE2902 of Characteristic 2
 
-// Characteristic specfiic to this
-
+// Characteristic Specific
 BLEDescriptor* pDescr;                      // Pointer to Descriptor of Characteristic 1
-
 BLE2902* pBLE2902;                          // Pointer to BLE2902 of Characteristic 1
-
-// BLECharacteristic* pCharacteristic = NULL;  // Pointer to Characteristic
-
-BLECharacteristic* pCharacteristicChars[8] = {NULL};
+BLECharacteristic* pCharacteristicChars[9] = {NULL};
 
 const char* characteristicUUIDs[] = {
   "beb5483e-36e1-4688-b7f5-ea07361b26a8",
   "1c95d5e3-d8f7-413a-bf3d-7a2e5d7be87e",
   "d7d85823-5304-4eb3-9671-3e571fac07b9",
   "d2789cef-106f-4660-9e3f-584c12e2e3c7",
+};
+
+const char* UUIDLabels[] = {
+  "TimeStamp",
+  "Latitude",
+  "Longitude",
+  "Altitude",
+  "PM25",
+  "RelativeHumidity",
+  "Temperature",
+  "AccelerationX",
+  "AccelerationY",
+  "AccelerationZ"
 };
 
 class MyServerCallbacks : public BLEServerCallbacks {
@@ -40,16 +48,16 @@ class MyServerCallbacks : public BLEServerCallbacks {
   };
 };
 
-BLECharacteristic* intializeBLECharacteristic(BLECharacteristic* pCharacteristic, BLEService* pService, const char*  characteristicUUID){
+BLECharacteristic* intializeBLECharacteristic(BLECharacteristic* pCharacteristic, BLEService* pService, const char*  characteristicUUID,const char*  label){
   // 2.a. Characteristics Here 
   pCharacteristic = pService->createCharacteristic(
   characteristicUUID,
   BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
   // BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
   
-  // 2.b.i Create a BLE Descriptor
+  // 2.b.i Create a BLE Descriptor #FIXME: descriptors not working correctly, at least I cant see the label in NRF Connect
   pDescr = new BLEDescriptor((uint16_t)0x2901);  //The 0x2901 is the Bluetooth SIG-defined UUID for the "Characteristic User Description" descriptor. This descriptor is used to provide a human-readable description or name for the characteristic.
-  pDescr->setValue("Test Value");
+  pDescr->setValue(label);
   pCharacteristic->addDescriptor(pDescr);
 
   //2.b.ii Add the BLE2902 Descriptor because we are using "PROPERTY_NOTIFY"
@@ -70,9 +78,10 @@ void setup() {
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
   BLEService* pService = pServer->createService(BLESERVICE_UUID, 30, 0);
+  
   // 2. Set Characteristics 
   for (int i = 0; i < 4; i ++){
-  pCharacteristicChars[i] = intializeBLECharacteristic(pCharacteristicChars[i], pService, characteristicUUIDs[i]);
+  pCharacteristicChars[i] = intializeBLECharacteristic(pCharacteristicChars[i], pService, characteristicUUIDs[i], UUIDLabels[i]);
   }
 
   // 3. Start the service
@@ -85,6 +94,9 @@ void setup() {
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
   Serial.println("Waiting a client connection to notify...");
+
+
+
 }
 
 void loop() {
@@ -109,7 +121,7 @@ void loop() {
   }
   // Connecting
   if (deviceConnected && !oldDeviceConnected) {     // do stuff here on connecting
-    Serial.print("Device Connected");
+    Serial.println("Device Connected");
     oldDeviceConnected = deviceConnected;
   }
 }
--- BLE_Implementation/BLE_Implementation.ino
+++ BLE_Implementation/BLE_Implementation.ino
@@ -17,72 +17,6 @@ unsigned long previousMillis = 0;
 const unsigned long sampleRate = 1000;
 
 // ---------------------------------------------------------------//
-// ------------------ BLE information ----------------------------//
-// ---------------------------------------------------------------//
-
-// #FIXME - add more pointers Initialize all pointers
-BLEServer* pServer = NULL;                    // Pointer to the server
-BLECharacteristic* pCharacteristic_1 = NULL;  // Pointer to Characteristic 1
-BLECharacteristic* pCharacteristic_2 = NULL;  // Pointer to Characteristic 2
-BLEDescriptor* pDescr_1;                      // Pointer to Descriptor of Characteristic 1
-BLE2902* pBLE2902_1;                          // Pointer to BLE2902 of Characteristic 1
-BLE2902* pBLE2902_2;                          // Pointer to BLE2902 of Characteristic 2
-
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-// UUIDs used in this example:
-static BLEUUID BLESERVICE_UUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
-// #define CHARACTERISTIC_UUID_1 "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-// #define CHARACTERISTIC_UUID_2 "1c95d5e3-d8f7-413a-bf3d-7a2e5d7be87e"
-
-const char* characteristicUUIDs[] = {
-  "beb5483e-36e1-4688-b7f5-ea07361b26a8",
-  "1c95d5e3-d8f7-413a-bf3d-7a2e5d7be87e",
-};
-
-
-
-const char* dataLabelList[] = {
-  "TimeStamp",
-  "Latitude",
-  "Longitude",
-};
-
-const char* sampleRateList[] = {
-  "5000",
-  "5000",
-  "5000",
-};
-
-struct sensorDataItem {
-  std::string UUID;
-  std::string label;
-  std::string sampleRate;
-};
-
-
-// Some variables to keep track on device connected
-const int numCharacteristics = sizeof(characteristicUUIDs) / sizeof(characteristicUUIDs[0]);
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-float value = 0.0f;
-float value_2 = 0.0f;
-int packet_count = 0;
-
-
-// Callback function that is called whenever a client is connected or disconnected
-class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-    deviceConnected = true;
-  };
-
-  void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
-  }
-};
-
-
-// ---------------------------------------------------------------//
 // ------------------ GPS information ----------------------------//
 // ---------------------------------------------------------------//
 #define GPSSerial Serial1
@@ -150,52 +84,106 @@ GPSData readAndStoreGPS() {
   }
 }
 
+// ---------------------------------------------------------------//
+// ------------------ BLE information ----------------------------//
+// ---------------------------------------------------------------//
+
+// Initialized Once // Not Characterisitic Specific  
+static BLEUUID BLESERVICE_UUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+const int numServices = 30;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+BLEServer* pServer = NULL;                    // Pointer to the server
+int value = 0;
+unsigned long deviceRebootMillis = 0;
+
+
+// Characteristic Specific
+BLEDescriptor* pDescr;                      // Pointer to Descriptor of Characteristic 1
+BLE2902* pBLE2902;                          // Pointer to BLE2902 of Characteristic 1
+BLECharacteristic* pCharacteristicChars[9] = {NULL};
+
+const char* characteristicUUIDs[] = {
+  "beb5483e-36e1-4688-b7f5-ea07361b26a8",
+  "1c95d5e3-d8f7-413a-bf3d-7a2e5d7be87e",
+  "d7d85823-5304-4eb3-9671-3e571fac07b9",
+  "d2789cef-106f-4660-9e3f-584c12e2e3c7",
+  "bf5a799d-26d0-410e-96b0-9ada1eb9f758",
+  "c22b405e-2b7b-4632-831d-54523e169a01",
+  "ffdda8ad-60a2-4184-baff-5c79a2eccb8c",
+  "183b971a-79f5-4004-8182-31c88d910dca",
+  "90b77f62-003d-454e-97fc-8f597b42048c",
+  "86cef02b-8c15-457b-b480-52e6cc0bdd8c"
+};
+
+const char* UUIDLabels[] = {
+  "TimeStamp",
+  "Latitude",
+  "Longitude",
+  "Altitude",
+  "PM25",
+  "RelativeHumidity",
+  "Temperature",
+  "AccelerationX",
+  "AccelerationY",
+  "AccelerationZ"
+};
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  };
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  };
+};
+
+BLECharacteristic* intializeBLECharacteristic(BLECharacteristic* pCharacteristic, BLEService* pService, const char*  characteristicUUID,const char*  label){
+  // 2.a. Characteristics Here 
+  pCharacteristic = pService->createCharacteristic(
+  characteristicUUID,
+  BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
+  // BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+  
+  // 2.b.i Create a BLE Descriptor #FIXME: descriptors not working correctly, at least I cant see the label in NRF Connect
+  pDescr = new BLEDescriptor((uint16_t)0x2901);  //The 0x2901 is the Bluetooth SIG-defined UUID for the "Characteristic User Description" descriptor. This descriptor is used to provide a human-readable description or name for the characteristic.
+  pDescr->setValue(label);
+  pCharacteristic->addDescriptor(pDescr);
+
+  //2.b.ii Add the BLE2902 Descriptor because we are using "PROPERTY_NOTIFY"
+  pBLE2902 = new BLE2902();
+  pBLE2902->setNotifications(true);
+  pCharacteristic->addDescriptor(pBLE2902);
+  return pCharacteristic;
+}
 
 void setup() {
 
   Serial.begin(115200);
 
+  //----------- GPS Set up-------------- //
   GPSSerial.begin(9600);  // #fixme (should I be worried about this?)
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);  // 1 Hz update rate
 
-
-  BLEDevice::init(deviceName);
-
-  // Create the BLE Server
+  //----------- BLE Set up-------------- //
+  // 1. Service information to run once   
+  BLEDevice::init(deviceName);     // You need a device 
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
   BLEService* pService = pServer->createService(BLESERVICE_UUID, 30, 0);
+  
+  // 2. Set Characteristics 
+  const int NumCharacteristics = sizeof(characteristicUUIDs) / sizeof(characteristicUUIDs[0]);
+  for (int i = 0; i < NumCharacteristics; i ++){
+  pCharacteristicChars[i] = intializeBLECharacteristic(pCharacteristicChars[i], pService, characteristicUUIDs[i], UUIDLabels[i]);
+  }
 
-  // Create a BLE Characteristic
-  pCharacteristic_1 = pService->createCharacteristic(
-    characteristicUUIDs[0],
-    BLECharacteristic::PROPERTY_NOTIFY);
-
-  pCharacteristic_2 = pService->createCharacteristic(
-    characteristicUUIDs[1],
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
-
-  // Create a BLE Descriptor
-  pDescr_1 = new BLEDescriptor((uint16_t)0x2901);  //The 0x2901 is the Bluetooth SIG-defined UUID for the "Characteristic User Description" descriptor. This descriptor is used to provide a human-readable description or name for the characteristic.
-  pDescr_1->setValue("Test Value");
-  pCharacteristic_1->addDescriptor(pDescr_1);
-
-  // Add the BLE2902 Descriptor because we are using "PROPERTY_NOTIFY"
-  pBLE2902_1 = new BLE2902();
-  pBLE2902_1->setNotifications(true);
-  pCharacteristic_1->addDescriptor(pBLE2902_1);
-
-  pBLE2902_2 = new BLE2902();
-  pBLE2902_2->setNotifications(true);
-  pCharacteristic_2->addDescriptor(pBLE2902_2);
-
-  // Start the service
+  // 3. Start the service
   pService->start();
 
-  // Start advertising
+  // 4. Start advertising
   BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(BLESERVICE_UUID);
   pAdvertising->setScanResponse(false);
@@ -206,76 +194,30 @@ void setup() {
 
 GPSData GPS2Transmit;
 void loop() {
-
   // notify changed value
   GPSData DummyGPSData = readAndStoreGPS();  // Hello GPS are you still there
+  
   if (DummyGPSData.dataReceived == true) {
     GPSData GPS2Transmit = DummyGPSData;
     Serial.println(GPS2Transmit.ShortTimeStamp);
   }
- if ( (millis() - GlobalTimer > sampleRateGps)) {
-  // if ((deviceConnected) && (millis() - GlobalTimer > sampleRateGps)) {
+
+ if  ((deviceConnected) && (millis() - GlobalTimer > sampleRateGps)) {
     GlobalTimer = millis();
     std::string DummyTimestamp = "04:32:27 13/06/0023";
-    // Serial.println(GPS2Transmit.ShortTimeStamp);
-    // Serial.print(GPS.seconds);
-    // Serial.println("-> seconds! ");
-
-
-    pCharacteristic_1->setValue(DummyTimestamp);
-    // pCharacteristic_1->setValue(std::string myString(GPS2Transmit.ShortTimeStamp))
-    pCharacteristic_1->notify();
-    value++;
-    // Serial.println(value_2);
-    value_2--;
-    std::string value_2_string = std::to_string(value_2);
-
-    // // Here the value is written to the Client using setValue();
-    // String txValue = "String with random value from Server: " + String(random(1000));
-    pCharacteristic_2->setValue(value_2_string);
-    pCharacteristic_2->notify();
+    pCharacteristicChars[0]->setValue(DummyTimestamp);
+    pCharacteristicChars[0]->notify();
   }
-  // The code below keeps the connection status uptodate:
+  
   // Disconnecting
-  if (!deviceConnected && oldDeviceConnected) {
-    //  delay(500); // #Fix me and remove delay
+  if (!deviceConnected && oldDeviceConnected && (millis() - deviceRebootMillis > 500) ) {
+    deviceRebootMillis = millis();
     pServer->startAdvertising();  // restart advertising
     Serial.println("start advertising");
     oldDeviceConnected = deviceConnected;
   }
   // Connecting
   if (deviceConnected && !oldDeviceConnected) {
-    // do stuff here on connecting
     oldDeviceConnected = deviceConnected;
   }
 }
-
-
-// GRAVEYARD
-
-// class SensorObject {
-//   public:
-
-//   SensorObject(char* UUID, char* label, int SampleRate){
-//     this->UUID = UUID;
-//     this->label = label;
-//     this->SampleRate = SampleRate;
-//   }
-
-//   // Member Attributes
-//   char UUID;
-//   char label;
-//   int SampleRate;
-// };
-
-// Serial.println("Characteristic 2 (setValue): " + txValue);
-
-// pCharacteristic_2 is a std::string (NOT a String). In the code below we read the current value
-// write this to the Serial interface and send a different value back to the Client
-// Here the current value is read using getValue()
-// std::string rxValue = pCharacteristic_2->getValue();
-// Serial.print("Characteristic 2 (getValue): ");
-// Serial.println(rxValue.c_str());
-
-// FIXME:  remove delay, GPS code wont work like this
-// delay(1000);
