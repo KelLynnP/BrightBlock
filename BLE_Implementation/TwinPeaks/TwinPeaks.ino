a4357ef1a88e846c714f960c5ec105978ed6334e
Kelsey Pool <kelseylp71@gmail.com>
2023-09-04 20:45:53 -0700

    Adding first Memory card functions

--- BLE_Implementation/TwinPeaks/TwinPeaks.ino
+++ BLE_Implementation/TwinPeaks/TwinPeaks.ino
@@ -29,6 +29,10 @@ const unsigned long sampleRate = 1000;
 const int NumCharacteristics = 10;
 const boolean isMemoryCardAttached = true;
 const int memoryCardPin = 27;
+int eventCounter = -1;
+bool isEventStarted = false;
+const char eventIndexPath []= "/eventIndex.txt";
+
 
 // -------------------- Which Sensors! ------------------------------//
 
@@ -59,168 +63,6 @@ const char* characteristicUUIDs[] = {
   "90b77f62-003d-454e-97fc-8f597b42048c",
   "86cef02b-8c15-457b-b480-52e6cc0bdd8c"
 };
-
-// ---------------------------------------------------------------//
-// ------------------ Memory Card Stuff --------------------------//
-// ---------------------------------------------------------------//
-
-// #FixME move into its own header file
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-  Serial.printf("Listing directory: %s\n", dirname);
-
-  File root = fs.open(dirname);
-  if(!root){
-    Serial.println("Failed to open directory");
-    return;
-  }
-  if(!root.isDirectory()){
-    Serial.println("Not a directory");
-    return;
-  }
-
-  File file = root.openNextFile();
-  while(file){
-    if(file.isDirectory()){
-      Serial.print("  DIR : ");
-      Serial.println(file.name());
-      if(levels){
-        listDir(fs, file.name(), levels -1);
-      }
-    } else {
-      Serial.print("  FILE: ");
-      Serial.print(file.name());
-      Serial.print("  SIZE: ");
-      Serial.println(file.size());
-    }
-    file = root.openNextFile();
-  }
-}
-
-void createDir(fs::FS &fs, const char * path){
-  Serial.printf("Creating Dir: %s\n", path);
-  if(fs.mkdir(path)){
-    Serial.println("Dir created");
-  } else {
-    Serial.println("mkdir failed");
-  }
-}
-
-void removeDir(fs::FS &fs, const char * path){
-  Serial.printf("Removing Dir: %s\n", path);
-  if(fs.rmdir(path)){
-    Serial.println("Dir removed");
-  } else {
-    Serial.println("rmdir failed");
-  }
-}
-
-void readFile(fs::FS &fs, const char * path){
-  Serial.printf("Reading file: %s\n", path);
-
-  File file = fs.open(path);
-  if(!file){
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-
-  Serial.print("Read from file: ");
-  while(file.available()){
-    Serial.write(file.read());
-  }
-  file.close();
-}
-
-void writeFile(fs::FS &fs, const char * path, const char * message){
-  Serial.printf("Writing file: %s\n", path);
-
-  File file = fs.open(path, FILE_WRITE);
-  if(!file){
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-  if(file.print(message)){
-    Serial.println("File written");
-  } else {
-    Serial.println("Write failed");
-  }
-  file.close();
-}
-
-void appendFile(fs::FS &fs, const char * path, const char * message){
-  Serial.printf("Appending to file: %s\n", path);
-
-  File file = fs.open(path, FILE_APPEND);
-  if(!file){
-    Serial.println("Failed to open file for appending");
-    return;
-  }
-  if(file.print(message)){
-      Serial.println("Message appended");
-  } else {
-    Serial.println("Append failed");
-  }
-  file.close();
-}
-
-void renameFile(fs::FS &fs, const char * path1, const char * path2){
-  Serial.printf("Renaming file %s to %s\n", path1, path2);
-  if (fs.rename(path1, path2)) {
-    Serial.println("File renamed");
-  } else {
-    Serial.println("Rename failed");
-  }
-}
-
-void deleteFile(fs::FS &fs, const char * path){
-  Serial.printf("Deleting file: %s\n", path);
-  if(fs.remove(path)){
-    Serial.println("File deleted");
-  } else {
-    Serial.println("Delete failed");
-  }
-}
-
-void testFileIO(fs::FS &fs, const char * path){
-  File file = fs.open(path);
-  static uint8_t buf[512];
-  size_t len = 0;
-  uint32_t start = millis();
-  uint32_t end = start;
-  if(file){
-    len = file.size();
-    size_t flen = len;
-    start = millis();
-    while(len){
-      size_t toRead = len;
-      if(toRead > 512){
-        toRead = 512;
-      }
-      file.read(buf, toRead);
-      len -= toRead;
-    }
-    end = millis() - start;
-    Serial.printf("%u bytes read for %u ms\n", flen, end);
-    file.close();
-  } else {
-    Serial.println("Failed to open file for reading");
-  }
-
-
-  file = fs.open(path, FILE_WRITE);
-  if(!file){
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-
-  size_t i;
-  start = millis();
-  for(i=0; i<2048; i++){
-    file.write(buf, 512);
-  }
-  end = millis() - start;
-  Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-  file.close();
-}
 
 // ---------------------------------------------------------------//
 // ------------------ GPS information ----------------------------//
@@ -392,6 +234,7 @@ void printModuleVersions() {
         Serial.println(hardwareMinor);
     }
 }
+
 void printSerialNumber() {
     uint16_t error;
     char errorMessage[256];
@@ -408,18 +251,6 @@ void printSerialNumber() {
         Serial.println((char*)serialNumber);
     }
 }
-
-// // Sensirion Values
-// struct SnsrnData {     
-//   float massConcentrationPm1p0;
-//   float massConcentrationPm2p5;
-//   float massConcentrationPm4p0;
-//   float massConcentrationPm10p0;
-//   float ambientHumidity;
-//   float ambientTemperature;
-//   float vocIndex;
-//   float noxIndex;
-// }
 
 Adafruit_ICM20948 icm;  // Initialize the ICM209448 sensor object
 unsigned long PlantMillis = 0;
@@ -516,6 +347,67 @@ std::vector<std::string> PullAndTranscribeData(const GPSData& GPSData2Transmit) 
 const int buttonPin = 25;  // the number of the pushbutton pin
 int buttonState = 0;
 
+
+// ---------------------------------------------------------------//
+// ------------------ Memory Card Stuff --------------------------//
+// ---------------------------------------------------------------//
+
+// #FixME move into its own header file
+int PullLastEventIndex(fs::FS &fs, const char * Eventpath){
+  Serial.println("Starting PullLastEventIndex ");    
+  File file = fs.open(Eventpath, FILE_READ);
+  String line;
+  if(!file){
+      Serial.println("Failed to open file for appending");
+      return -1;
+  }
+
+  while (file.available()) {
+    line = file.readStringUntil(',');
+    Serial.println(line);
+  }
+
+  int numLine = std::stoi(line.c_str());  
+  Serial.printf("Appending to file: %s\n", Eventpath);
+  std::string numLinePlusOne = std::to_string(numLine + 1);
+  numLinePlusOne += ',';
+
+  if(!file){
+      Serial.println("Failed to open file for appending");
+      return -1;
+  }
+
+  if(file.print(numLinePlusOne.c_str())){
+      Serial.println("Message appended");
+  } else {
+      Serial.println("Append failed");
+  }
+  file.close();
+  return numLine;
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void saveLineData(std::string Row_Data, int eventCounter){
+
+return;
+}
+
+
 void setup() {
 
   Serial.begin(115200);
@@ -542,9 +434,8 @@ void setup() {
     } else {
       Serial.println("UNKNOWN");
     }
-
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
 
 
   //----------- GPS Set up-------------- //
@@ -707,10 +598,9 @@ void setup() {
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
   Serial.println("Waiting a client connection to notify...");
-  bool isEventStarted = false;
+
 
 }
-
 
 GPSData GPS2Transmit;
 
@@ -727,13 +617,24 @@ void loop() {
     GlobalTimer = millis();
     std::vector<std::string> EncodedData = PullAndTranscribeData(GPS2Transmit);
     
-
+    std::string Row_Data; 
     for (int i = 0; i < NumCharacteristics; i++) {  //
-      Serial.print(i);
-      Serial.println(EncodedData[i].c_str());
+      // Serial.print(i);
+      // Serial.println(EncodedData[i].c_str());
       pCharacteristicChars[i]->setValue(std::string(EncodedData[i]));
       pCharacteristicChars[i]->notify();
+      Row_Data += EncodedData[i].c_str();
+      // Row_Data += ',';
+      // Serial.println(Row_Data.c_str());
     }
+    Serial.println(Row_Data.c_str());
+
+    Row_Data += '\n';
+    if (isMemoryCardAttached){
+      
+      // saveLineData(Row_Data, eventCounter);
+    }
+
   }
 
 
@@ -746,20 +647,19 @@ void loop() {
     oldDeviceConnected = deviceConnected;
     isEventStarted = false;
   }
+
   // Connection
   if (deviceConnected && !oldDeviceConnected) {
     oldDeviceConnected = deviceConnected;
     
-    if isEventStarted == false{
-       listDir(SD, "/", 0);
-      writeFile(SD, "/hello.txt", "Hello ");
-
-      
-      isEventStarted
-
+    // pull last Event Started
+    if (isEventStarted == false){ 
+      eventCounter = PullLastEventIndex(SD, eventIndexPath);
+      // newPath = sprintf("\Event")
+      // writeFile(SD, eventIndexPath)
+      isEventStarted = true;
     }
 
   }
-
 
 }
